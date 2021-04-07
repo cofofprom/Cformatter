1f526270807f7a9407eefb634c4e2967b69d47ff
ArtNlk <ArtNlk@users.noreply.github.com>
2021-04-08 00:43:43 +0300

    Set up tests and fixed bugs in string array

--- StringArray/stringarray.c
+++ StringArray/stringarray.c
@@ -6,15 +6,15 @@ STRING_ARRAY* initStringArray()
     newArray = calloc(1,sizeof(STRING_ARRAY));
     if(newArray == NULL)
     {
-        printf("Error! New string array allocation error");
+        //printf("Error! New string array allocation error");
         return NULL;
     }
 
     newArray->length = 0;
     newArray->strings = calloc(STRINGARRAY_INITIAL_ALLOCATED_LENGTH,sizeof(char*));
-    if(newArray->strings = NULL)
+    if(newArray->strings == NULL)
     {
-        printf("Error! New string array strings field allocation error!");
+        //printf("Error! New string array strings field allocation error!");
         free(newArray);
         return NULL;
     }
@@ -26,7 +26,7 @@ void freeStringArray(STRING_ARRAY* stringArray)
 {
     if(stringArray == NULL)
     {
-        printf("Error! Attempt to free a NULL string array!");
+        //printf("Error! Attempt to free a NULL string array!");
         return;
     }
     for(int i = 0; i < stringArray->length; i++)
@@ -45,14 +45,20 @@ void freeStringArray(STRING_ARRAY* stringArray)
 int appendString(char *inputString, STRING_ARRAY** stringArray) {
     if(stringArray == NULL)
     {
-        printf("WARNING: appendString: attempt to append string to a NULL pointed stringarray");
+        //printf("WARNING: appendString: attempt to append string to a NULL pointed stringarray");
         return EINVAL;
     }
 
     if(inputString == NULL)
     {
-        printf("WARNING: appendString: attempt to append a NULL string to stringarray");
+        //printf("WARNING: appendString: attempt to append a NULL string to stringarray");
         return EINVAL;
+    }
+
+    if(strlen(inputString) > STRINGARRAY_MAX_STRING_LENGTH)
+    {
+        //printf("WARNING: appendString: appended string is length is bigger than limit! (Limit is %d)",STRINGARRAY_MAX_STRING_LENGTH);
+        return EOVERFLOW;
     }
 
     if((*stringArray)->length == (*stringArray)->allocatedLength)
@@ -60,14 +66,20 @@ int appendString(char *inputString, STRING_ARRAY** stringArray) {
         STRING_ARRAY* newStringArray = realloc(*stringArray,(*stringArray)->allocatedLength*2);
         if(newStringArray == NULL)
         {
-            printf("WARNING: appendString: error reallocating bigger stringarray attempting to append a new string!");
+            //printf("WARNING: appendString: error reallocating bigger stringarray attempting to append a new string!");
             return ENOMEM;
         }
         *stringArray = newStringArray;
         (*stringArray)->allocatedLength *= 2;
     }
-
-    (*stringArray)->strings[(*stringArray)->length] = inputString;
+    char* newString = calloc(strlen(inputString)+1,sizeof(char));
+    if(newString == NULL)
+    {
+        //printf("WARNING: appendString: error allocating stringarray string attempting to append a new string!");
+        return ENOMEM;
+    }
+    int result = strcpy_s(newString,strlen(inputString)+1,inputString);
+    (*stringArray)->strings[(*stringArray)->length] = newString;
     (*stringArray)->length++;
     return 0;
 }
@@ -79,20 +91,20 @@ int appendString(char *inputString, STRING_ARRAY** stringArray) {
 int deleteStringAt(int index, STRING_ARRAY* stringArray) {
     if(stringArray == NULL)
     {
-        printf("WARNING: deleteStringAt: attempt to remove string from a NULL pointed stringarray!");
+        //printf("WARNING: deleteStringAt: attempt to remove string from a NULL pointed stringarray!");
         return EINVAL;
     }
 
     if(index >= stringArray->length || index < 0)
     {
-        printf("WARNING: deleteStringAt: deletion index out of bounds!");
+        //printf("WARNING: deleteStringAt: deletion index out of bounds!");
         return EINVAL;
     }
 
     if(stringArray->strings[index] == NULL)
     {
-        printf("WARNING: deleteStringAt: deletion index is in bounds, "
-               "but string pointer is NULL, possible memory corruption!");
+        //printf("WARNING: deleteStringAt: deletion index is in bounds, "
+        //       "but string pointer is NULL, possible memory corruption!");
         return EFAULT;
     }
 
@@ -115,22 +127,30 @@ int deleteStringAt(int index, STRING_ARRAY* stringArray) {
 const char* getStringAt(int index, STRING_ARRAY *stringArray) {
     if(stringArray == NULL)
     {
-        printf("WARNING: getStringAt: attempt to get string from a NULL pointed stringarray!");
-        return EINVAL;
+        //printf("WARNING: getStringAt: attempt to get string from a NULL pointed stringarray!");
+        return NULL;
     }
 
     if(index >= stringArray->length || index < 0)
     {
-        printf("WARNING: getStringAt: get index out of bounds!");
-        return EINVAL;
+        //printf("WARNING: getStringAt: get index out of bounds!");
+        return NULL;
     }
 
     if(stringArray->strings[index] == NULL)
     {
-        printf("WARNING: getStringAt: get index is in bounds, "
-               "but string pointer is NULL, possible memory corruption!");
-        return EFAULT;
+        //printf("WARNING: getStringAt: get index is in bounds, "
+        // "but string pointer is NULL, possible memory corruption!");
+        return NULL;
     }
 
-    return stringArray->strings[index];
+    char* newString = calloc(strlen(stringArray->strings[index])+1,sizeof(char));
+    if(newString == NULL)
+    {
+        //printf("WARNING: getStringAt: error allocating stringarray output string attempting to get string!");
+        return NULL;
+    }
+    strcpy_s(newString,strlen(stringArray->strings[index])+1,stringArray->strings[index]);
+
+    return newString;
 }
--- tests/stringarray/stringarrayTests.c
+++ tests/stringarray/stringarrayTests.c
@@ -3,19 +3,49 @@
 //
 // create a test...
 //
-void test_construction()
+void test_main()
 {
-    int reference = 2;
-    assert_int_equal(reference,dummy(reference));
+    char* testString1 = "String1";
+    char* testString2 = "String2";
+    char* testString3 = "String3";
+    STRING_ARRAY* referenceStringArray = initStringArray();
+    assert_false(referenceStringArray == NULL);
+    assert_true(referenceStringArray->length == 0);
+    assert_true(referenceStringArray->allocatedLength == 50);
+    int appendResult = appendString(testString1,&referenceStringArray);
+    assert_int_equal(0,appendResult);
+    assert_string_equal(testString1,referenceStringArray->strings[0]);
+    assert_string_equal(NULL,referenceStringArray->strings[1]);
+    appendResult = appendString(testString2,&referenceStringArray);
+    assert_int_equal(0,appendResult);
+    assert_string_equal(testString2,referenceStringArray->strings[1]);
+    assert_string_equal(NULL,referenceStringArray->strings[2]);
+    appendResult = appendString(NULL,&referenceStringArray);
+    assert_int_equal(EINVAL,appendResult);
+    appendResult = appendString(testString3,NULL);
+    assert_int_equal(EINVAL,appendResult);
+    int deleteResult = deleteStringAt(10,referenceStringArray);
+    assert_int_equal(EINVAL,deleteResult);
+    appendString(testString3,&referenceStringArray);
+    char* temp = referenceStringArray->strings[1];
+    referenceStringArray->strings[1] = NULL;
+    deleteResult = deleteStringAt(1,referenceStringArray);
+    assert_int_equal(EFAULT,deleteResult);
+    referenceStringArray->strings[1] = temp;
+    deleteResult = deleteStringAt(1,referenceStringArray);
+    assert_int_equal(0,deleteResult);
+    assert_string_equal(testString3,referenceStringArray->strings[1]);
+    assert_string_equal(testString3,getStringAt(1,referenceStringArray));
+    assert_string_equal(NULL,getStringAt(2,referenceStringArray));
 }
 
 //
 // put the test into a fixture...
 //
-void test_fixture_dummy( void )
+void test_fixture_main( void )
 {
     test_fixture_start();
-    run_test(test_construction());
+    run_test(test_main);
     test_fixture_end();
 }
 
@@ -24,7 +54,7 @@ void test_fixture_dummy( void )
 //
 void all_tests( void )
 {
-    test_fixture_dummy();
+    test_fixture_main();
 }
 
 //
