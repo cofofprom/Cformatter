dc14e91f0e886f3eba49ab74f7d6f4e75aad7fc2
Ailurus-code <76646255+Ailurus-code@users.noreply.github.com>
2021-04-07 13:30:43 +0300

    MaxLoopNesting upgraded

--- StringTools/StringTools.c
+++ StringTools/StringTools.c
@@ -41,21 +41,26 @@ char *replaceWord(char *s, const char *oldW, const char *newW, int ptr)
         else result[i++] = *str++;
     }
     result[i] = '\0';
-
-    for (int j = ptr, k = 0; k<strlen(result); j++, k++)
+    int j, k = 0;
+    for (j = ptr, k = 0; k<strlen(result); j++, k++)
     {
         source[j] = result[k];
     }
-
+    for (int t=strlen(source)-1; t>=j; t--)
+    {
+        source[t] = '\0';
+    }
     return source;
 }
 
 char *replaceOneWord(char *s, const char *oldW, const char *newW, int ptr)
 {
-    char result[SIZE] = {0}, temp[SIZE] = {0}, *source = (char*)malloc(strlen(s)+1) , *str = (char*)malloc(strlen(s)+1);
+    int newWlen = strlen(newW);
+    int oldWlen = strlen(oldW);
+    char result[SIZE] = {0}, temp[SIZE] = {0}, *source = (char *) malloc(strlen(s) + (newWlen - oldWlen) + 1), *str = (char*)malloc(strlen(s)+1);
     strcpy(str, s);
     int idx = 0;
-    int len = strlen(str);
+    int len = strlen(s);
     for (int k = ptr; k < len; k++)
     {
         temp[idx] = str[k];
@@ -64,8 +69,6 @@ char *replaceOneWord(char *s, const char *oldW, const char *newW, int ptr)
     strcpy(source, str);
     strcpy(str, temp);
     int i, cnt = 0;
-    int newWlen = strlen(newW);
-    int oldWlen = strlen(oldW);
     for (i = 0; str[i] != '\0'; i++)
     {
         if (strstr(&str[i], oldW) == &str[i])
@@ -88,9 +91,14 @@ char *replaceOneWord(char *s, const char *oldW, const char *newW, int ptr)
         else result[i++] = *str++;
     }
     result[i] = '\0';
-    for (int j = ptr, k = 0; k<strlen(result); j++, k++)
+    int j, k = 0;
+    for (j = ptr, k = 0; k<strlen(result); j++, k++)
     {
         source[j] = result[k];
+    }
+    for (int t=strlen(source)-1; t>=j; t--)
+    {
+        source[t] = '\0';
     }
     return source;
 }
